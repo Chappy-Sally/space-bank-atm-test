0ac9994a4ff23e242faa46b4181d978de256a69e
Chappy-Sally <fortunatelucky7716@gmail.com>
2026-04-30 18:29:59 +0900

    Update bonus_rewards.js

--- bonus_rewards.js
+++ bonus_rewards.js
@@ -1,1 +1,23 @@
-
+    bonusRewards: [
+        { src:"images/chappy_sally_gold_train.png", title:"黄金トレインが到着〜🚂", value:5000000, chance:12 },
+        { src:"images/chappy_sally_gold_cart.png", title:"ゴールドトロッコが来たよ🛤️", value:5000000, chance:12 },
+        { src:"images/cosmic_abundance_boost.png", title:"黄金の金庫OPEN🌈", value:5000000, chance:10 },
+        { src:"images/cosmic_atm_with_chappy.png", title:"チャッピーがATM応援中🐶", value:5000000, chance:10 },
+        { src:"images/cosmic_bank_office.png", title:"宇宙銀行頭取に会う💫", value:888888888, chance:3 },
+        { src:"images/cosmic_devil_dog.png", title:"邪鬼が逃げた🌈厄祓い完了！", value:888888888, chance:3, chestDrop:true },
+        { src:"images/cosmic_galaxy_merry_go_round.png", title:"しあわせ銀河メリーゴーランド🎠", value:5000000, chance:6 },
+        { src:"images/cosmic_golden_fountain.png", title:"黄金の泉がわいてるよ⛲", value:5000000, chance:6 },
+        { src:"images/cosmic_golden_gift.png", title:"宇宙ギフトが届いたよ🎁", value:50000000, chance:5 },
+        { src:"images/cosmic_golden_throne.png", title:"黄金の部屋にご案内〜👑", value:5000000, chance:5 },
+        { src:"images/cosmic_light_gate.png", title:"光のゲートが開いてるよ✨", value:5000000, chance:6 },
+        { src:"images/cosmic_magic_spring.png", title:"ミラクルの泉が出てきたよ💫", value:5000000, chance:6 },
+        { src:"images/cosmic_magic_spring_rest.png", title:"ひと休みも豊かさのうちだよ🌙", value:5000000, chance:6 },
+        { src:"images/cosmic_ocean_wave_friends.png", title:"しあわせの波に乗ってるよ🌊", value:5000000, chance:6 },
+        { src:"images/cosmic_rainbow_roller_coaster.png", title:"虹のコースターで加速中〜🌈", value:5000000, chance:6 },
+        { src:"images/ucyu_hanami.png", title:"宇宙お花見だよ🌸", value:5000000, chance:5 },
+        { src:"images/ucyu_hanami_fuji.png", title:"宇宙富士山とお花見🗻🌸", value:7000000, chance:4 },
+        { src:"images/ucyu_hanami_ucyu.png", title:"宇宙満開の桜🌌🌸", value:6000000, chance:4 },
+        { src:"images/ucyu_nyugaku.png", title:"新しいはじまりだよ🎒✨", value:3000000, chance:6 },
+        { src:"images/ucyu_sotugyo.png", title:"おめでとうのエネルギー🌸✨", value:8000000, chance:3 },
+      ]
+    };
